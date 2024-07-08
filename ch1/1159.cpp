5eef6143821773d5068f4677e1c2ebc7feb4ccf6
KKJ <kyujin0911@tukorea.ac.kr>
2024-07-08 15:49:19 +0900

    1159: 농구 경기 공간복잡도 최적화

--- ch1/1159.cpp
+++ ch1/1159.cpp
@@ -1,16 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int N;
-string s[154];
-int cnt[26];
-string result;
+int N, cnt[26];
+string s, result;
 
 int main(){
     cin >> N;
     for(int i = 0; i < N; i++){
         cin >> s[i];
-        cnt[s[i][0] - 97]++;
+        cnt[s[0] - 97]++;
     }
 
     for(int i = 0; i < 26; i++){
